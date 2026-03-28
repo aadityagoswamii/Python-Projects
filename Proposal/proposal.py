2d4d8d8c3f94bbee08a2c13c1fe441c8535916e4
aadityagoswamii <adityagiriindia2nd@gmail.com>
2026-03-28 14:25:50 +0530

    organized project structure

---
+++ Proposal/proposal.py
@@ -0,0 +1,11 @@
+
+        while True:
+
+            proposal = input("Will you marry mw\e???(Y for yes)")
+
+            if proposal == "yes" or proposal == "Yes" or proposal == "Y" or proposal == "y":
+                print("omg i love you")
+                break
+
+            else:
+                print("Think again")
